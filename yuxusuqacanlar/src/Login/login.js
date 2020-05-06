a9aa7794a333b2220f87a05c94f2d6775d285ca2
Tagizade Orxan <tagizade.orxan@gmail.com>
2020-05-06 22:27:08 +0400

    ok

--- yuxusuqacanlar/src/Login/login.js
+++ yuxusuqacanlar/src/Login/login.js
@@ -29,7 +29,7 @@ class LoginComponent extends React.Component {
 
     return (
       <main className={classes.main}>
-      <h1 className="headtext">Yuxusu Qaçanlar</h1>
+      <h1 className="headtext">Yuxusu Qaçanlar by Orxan Tagizade</h1>
         <CssBaseline/>
         <Paper className={classes.paper}>
           <Typography component="h1" variant="h5">
